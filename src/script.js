d5d526c33d27dc995bd1441322f63311a876d253
Akshay Rathod <kr.akshay234@gmail.com>
2023-06-13 19:11:20 +0530

    updated files

--- src/script.js
+++ src/script.js
@@ -1,20 +1,26 @@
-// A callback function to read valid and invalid emails from a csv file and display them in a table
-function readEmails() {
-    var file = document.getElementById("file").files[0];
+// Read valid and invalid emails from a csv file
+document.getElementById("file-input").addEventListener("change", function () {
+    console.log("file-input change event");
+    var file = document.getElementById("file-input").files[0];
     var reader = new FileReader();
-    var emails = [];
+    var validEmails = [];
     var invalidEmails = [];
+    reader.readAsText(file);
     reader.onload = function (e) {
         var text = e.target.result;
         var lines = text.split("\n");
         for (var i = 0; i < lines.length; i++) {
-            if (validateEmail(lines[i])) {
-                emails.push(lines[i]);
+            var email = lines[i].trim();
+            var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,3}$/
+            if (emailRegex.test(email)) {
+                validEmails.push(email);
             } else {
-                invalidEmails.push(lines[i]);
+                invalidEmails.push(email);
             }
         }
-        displayEmails(emails, invalidEmails);
+        document.getElementById("validEmails").innerHTML = validEmails
+        document.getElementById("invalidEmails").innerHTML = invalidEmails
+        document.getElementById("validEmailCount").innerText = "(" + validEmails.length + ")";
+        document.getElementById("invalidEmailCount").innerText = "(" + invalidEmails.length + ")";
     };
-    reader.readAsText(file);
-}
+});
